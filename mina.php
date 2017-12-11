f926506024f9c40652e26d35587fe33b46993e91
tootma1995 <norman.kukk@khk.ee>
2017-12-11 12:40:39 +0200

    Defineeritud ja väljastatud muutuja

--- mina.php
+++ mina.php
@@ -16,4 +16,13 @@ echo 'Echo: Minu nimi on ' . $norman . ".<br/>"; //käsk
 print 'Print: Minu nimi on '. $norman . '.<br/>'; //funktsioon
 print("Print(): Minu nimi on $norman."); // kahekordsed jutumärkidega näeb muutuja väärtust
 
+$var1 = 5;
+$var2 = 5.0;
+$var3 = '5';
+$var4 = 2;
+$var5 = 2.0;
+$var6 = '2';
+$var7 = true;
+$var8 = false;
+
 ?>
