fd6a9f41370671bae62bf81579416f17974b7f48
tootma1995 <norman.kukk@khk.ee>
2017-12-11 12:44:48 +0200

    Lisatud 8 muutujat ja mis nende tüüp on

--- mina.php
+++ mina.php
@@ -25,4 +25,13 @@ $var6 = '2';
 $var7 = true;
 $var8 = false;
 
+echo $var1 . ' - ' . gettype($var1) . '<br/>';
+echo $var2 . ' - ' . gettype($var2) . '<br/>';
+echo $var3 . ' - ' . gettype($var3) . '<br/>';
+echo $var4 . ' - ' . gettype($var4) . '<br/>';
+echo $var5 . ' - ' . gettype($var5) . '<br/>';
+echo $var6 . ' - ' . gettype($var6) . '<br/>';
+echo $var7 . ' - ' . gettype($var7) . '<br/>';
+echo $var8 . ' - ' . gettype($var8) . '<br/>';
+
 ?>
