0a01229fa9c2ce17e7049431d97554b913cd1bf1
tootma1995 <norman.kukk@khk.ee>
2017-12-15 13:13:29 +0200

    Varviline txt

---
+++ varviline_txt.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: norsi
+ * Date: 15/12/2017
+ * Time: 13:02
+ */
+
+header('Refresh:0.2');
+for($rida=1;$rida<=5;$rida++){
+    $varv = '#';
+    for ($kord = 1; $kord <= 6; $kord++){
+        $juhuTaisarv = rand(0,15);
+        $juhuHex=dechex($juhuTaisarv);
+        $varv = $varv.$juhuHex;
+    }
+    echo '<font color="'.$varv.'"> Värviline tekst </font><br/>'; // ei saa aru color="'.$varv.'"
+}
+
+?>
