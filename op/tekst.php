00ef66c8f5d5ba7b8d9fbfc8a6e54d1266548713
tootma1995 <norman.kukk@khk.ee>
2018-01-15 14:32:28 +0200

    loodud klassi algkirjeldus ja testitud objekti loomist

---
+++ op/tekst.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Norman
+ * Date: 15/01/2018
+ * Time: 14:21
+ */
+
+class tekst
+{
+    // klassi muutujad, omadused, variables
+    var $sonad = '';
+}
---
+++ op/tekst_test.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Norman
+ * Date: 15/01/2018
+ * Time: 14:27
+ */
+require_once 'tekst.php';
+
+// loome teksti objekti
+$minuTekst = new tekst();
+
+// väljastame objekti sisu test kujul
+echo '<pre>';
+print_r($minuTekst);
+echo '</pre>';
