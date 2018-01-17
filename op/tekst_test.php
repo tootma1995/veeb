96356973a7e7b3c72d41407bcf8668e07fb1fcf7
tootma1995 <norman.kukk@khk.ee>
2018-01-17 11:38:55 +0200

    pisiparandus

--- op/tekst_test.php
+++ op/tekst_test.php
@@ -9,7 +9,7 @@ require_once 'tekst.php';
 
 // loome teksti objekti
 // määrame kindla sõnad tekstist
-$minuTekst = new tekst('Tere maailm');
+$minuTekst = new tekst('Tere maailm!');
 
 // väljastame objekti sisu test kujul
 echo '<pre>';
@@ -19,13 +19,15 @@ echo '</pre>';
 // väljastame objekti korralikult
 $minuTekst->prindiTekst();
 
+
+
 echo '<hr/>';
 
 require_once 'varvilineTekst.php';
 
 // loome teksti objekti
 // määrame kindla sõnad tekstist
-$varvituTekst = new tekst('Tere maailm');
+$varvituTekst = new varvilineTekst('Tere maailm!');
 
 // väljastame objekti sisu test kujul
 echo '<pre>';
@@ -42,7 +44,7 @@ require_once 'varvilineTekst.php';
 
 // loome teksti objekti
 // määrame kindla sõnad tekstist
-$roosaTekst = new varvilineTekst('Tere maailm','#ff0066');
+$roosaTekst = new varvilineTekst('Tere maailm!','#ff0066');
 
 // väljastame objekti sisu test kujul
 echo '<pre>';
